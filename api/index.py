7da5cbd351f4c55e71babf15b97e10a291466d06
Elijah Siaw <elijahsiaw@gmail.com>
2025-10-06 21:41:44 +0000

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import sys
 import io
@@ -11,7 +11,7 @@ def apiHandler(key):
   return apiKey==key
 @app.route('/')
 def home():
-  return 'Python flask server'
+  return render_template('index.html')
 @app.route('/api/run', methods=['POST'])
 def run():
   try:
